1f85b1950d75c2a23438d4dee543d24e94d01e3f
rajendra <nagabhoinarajendra@gmail.com>
2018-06-16 18:44:02 +0530

    first commit

---
+++ bubble_sort/src/test/java/bubble_sort/SortTest1.java
@@ -0,0 +1,14 @@
+package bubble_sort;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SortTest1 {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ merge_sort/src/test/java/merge_sort/SortTest1.java
@@ -0,0 +1,14 @@
+package merge_sort;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SortTest1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
